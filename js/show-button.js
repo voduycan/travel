049a04348ecb4c2ddc66309fcad824c695898fa0
voduycan <voduycan94@gmail.com>
2018-06-26 15:35:48 +0700

    search result

--- js/show-button.js
+++ js/show-button.js
@@ -1,4 +1,5 @@
 var isShow = true;
+ var isPoor = isFair = isGood = isVeryGood = isExcellent = true;
 $(document).ready(function(){
     $('#btn-more').click(function(){
         $('#less').removeClass('btn-hide');
@@ -21,4 +22,156 @@ $(document).ready(function(){
         }
         isShow = !isShow;
     });
+    $('#poor').click(function(){
+        if(isPoor){
+            $('#poor').addClass('color-red');
+        }
+        else{
+            $('#poor').removeClass('color-red');
+        }
+        isPoor = !isPoor;
+    });
+    $('#fair').click(function(){
+        if(isFair){
+            $('#fair').addClass('color-red');
+        }
+        else{
+            $('#fair').removeClass('color-red');
+        }
+        isFair = !isFair;
+    });
+    $('#good').click(function(){
+        if(isGood){
+            $('#good').addClass('color-red');
+        }
+        else{
+            $('#good').removeClass('color-red');
+        }
+        isGood = !isGood;
+    });
+    $('#very-good').click(function(){
+        if(isVeryGood){
+            $('#very-good').addClass('color-red');
+        }
+        else{
+            $('#very-good').removeClass('color-red');
+        }
+        isVeryGood = !isVeryGood;
+    });
+    $('#excellent').click(function(){
+        if(isExcellent){
+            $('#excellent').addClass('color-red');
+        }
+        else{
+            $('#excellent').removeClass('color-red');
+        }
+        isExcellent = !isExcellent;
+    });
 });
+
+    $('#str-5').hover(function(){
+        $('#str-5').addClass('str-gold');
+        $('#str-4').addClass('str-gold');
+        $('#str-3').addClass('str-gold');
+        $('#str-2').addClass('str-gold');
+    });
+    $('#str-4').hover(function(){
+        $('#str-5').removeClass('str-gold');
+        $('#str-4').addClass('str-gold');
+        $('#str-3').addClass('str-gold');
+        $('#str-2').addClass('str-gold');
+    });
+    $('#str-3').hover(function(){
+        $('#str-5').removeClass('str-gold');
+        $('#str-4').removeClass('str-gold');
+        $('#str-3').addClass('str-gold');
+        $('#str-2').addClass('str-gold');
+    });
+    $('#str-2').hover(function(){
+        $('#str-5').removeClass('str-gold');
+        $('#str-4').removeClass('str-gold');
+        $('#str-3').removeClass('str-gold');
+        $('#str-2').addClass('str-gold');
+    });
+    $('#str-1').hover(function(){
+        $('#str-5').removeClass('str-gold');
+        $('#str-4').removeClass('str-gold');
+        $('#str-3').removeClass('str-gold');
+        $('#str-2').removeClass('str-gold');
+    });
+
+   
+
+  var str5=str4=str3=str2=false;
+    $('#str-5').click(function(){
+        str5 = true;
+        str4 = false;
+        str3 = false;
+        str2 = false;
+    });
+    $('#str-4').click(function(){
+        str5 = false;
+        str4 = true;
+        str3 = false;
+        str2 = false;
+    });
+    $('#str-3').click(function(){
+        str5 = false;
+        str4 = false;
+        str3 = true;
+        str2 = false;
+    });
+    $('#str-2').click(function(){
+        str5 = false;
+        str4 = false;
+        str3 = false;
+        str2 = true;
+    });
+    if(str5){
+        str5_show();
+    }
+    if(str4){
+        str4_show();
+    }
+    if(str3){
+        str3_show();
+    }
+    if(str2){
+        str2_show();
+    }
+    if(str1){
+        str1_show();
+    }
+    function str5_show(){
+        $('#str-5').addClass('str-gold');
+        $('#str-4').addClass('str-gold');
+        $('#str-3').addClass('str-gold');
+        $('#str-2').addClass('str-gold');
+    }
+    function str4_show(){
+        $('#str-5').removeClass('str-gold');
+        $('#str-4').addClass('str-gold');
+        $('#str-3').addClass('str-gold');
+        $('#str-2').addClass('str-gold');
+    }
+    function str3_show(){
+        $('#str-5').removeClass('str-gold');
+        $('#str-4').removeClass('str-gold');
+        $('#str-3').addClass('str-gold');
+        $('#str-2').addClass('str-gold');
+    }
+    function str2_show(){
+        $('#str-5').removeClass('str-gold');
+        $('#str-4').removeClass('str-gold');
+        $('#str-3').removeClass('str-gold');
+        $('#str-2').addClass('str-gold');
+    }
+    function str1_show(){
+        $('#str-5').removeClass('str-gold');
+        $('#str-4').removeClass('str-gold');
+        $('#str-3').removeClass('str-gold');
+        $('#str-2').removeClass('str-gold');
+    }
+
+
+    
--- js/slide.js
+++ js/slide.js
@@ -3,4 +3,15 @@ $(window).load(function() {
       animation: "slide",
       controlNav: "thumbnails"
     });
+
+  });
+ 
+  $(document).ready(function(){
+    $('#str-5').click(function(){
+      console.log('da hover');
+      $('#str-5').addClass('str-gold');
+      $('#str-4').addClass('str-gold');
+      $('#str-3').addClass('str-gold');
+      $('#str-2').addClass('str-gold');
+    });
   });
